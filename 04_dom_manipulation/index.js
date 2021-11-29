c0898b43e31c96e79661abb53b6e1f1cbd9ecf59
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-11-29 12:22:53 -0800

    end of part 1 lecture 4 on DOM

--- 04_dom_manipulation/index.js
+++ 04_dom_manipulation/index.js
@@ -72,43 +72,43 @@ function copy(obj) {
 // ✅ Searching for DOM elements
 
 function getSidebarElement() {
-
+  return document.getElementById('sidebar')
 }
 
 console.log('getSidebarElement', getSidebarElement())
 
 function getPlaylistElement() {
-  
+  return document.querySelector('#playlist')
 }
 
 console.log('getPlaylistElement', getPlaylistElement())
 
 function getMainElement() {
-  
+  return document.getElementById('main')
 }
 
 console.log('getMainElement', getMainElement())
 
 function getSongNameElement() {
-  
+  return document.getElementById('song-name')
 }
 
 console.log('getSongNameElement', getSongNameElement())
 
 function getArtistElement() {
-  
+  return document.getElementById('artist')
 }
 
 console.log('getArtistElement', getArtistElement())
 
 function getPlayCountElement() {
-  
+  return document.getElementById('play-count')
 }
 
 console.log('getPlayCountElement', getPlayCountElement())
 
 function getPlayerElement() {
-  
+  return document.getElementById('player')
 }
 
 console.log('getPlayerElement', getPlayerElement())
@@ -128,33 +128,57 @@ function renderSong(song) {
   return li;
 }
 
+function renderSong(song) {
+  const li = document.createElement('li');
+  li.className = "flex justify-between p-2 pr-4"
+  li.innerHTML = `
+  <div>
+    <span class="song font-semibold"></span>
+    <span class="artist"></span>
+  </div>
+  <div class="duration text-gray-400"></div>`
+  const songSpan = li.querySelector('.song');
+  const artistSpan = li.querySelector('.artist');
+  const durationEl = li.querySelector('.duration')
+  songSpan.textContent = song.name;
+  artistSpan.textContent = song.artist;
+  durationEl.textContent = formatDuration(song.duration);
+  song.element = li;
+  return li;
+}
+
 
 function loadPlaylistToSidebar() {
-  
+  const target = getPlaylistElement();
+  playlist.forEach(song => {
+    target.append(renderSong(song));
+  })
 }
 
 // // 👟👟👟 uncomment the line below to test
 
-// loadPlaylistToSidebar();
+loadPlaylistToSidebar();
 
 function addSongToPlaylist(playlist, song) {
   playlist.push(song);
   // Update the DOM with the new song in the sidebar
+  const target = getPlaylistElement();
+  target.append(renderSong(song));
   return song;
 }
 
 // // 👟👟👟 uncomment the lines below to test
 
-// window.setTimeout(() => {
-//   console.log('addSongToPlaylist', addSongToPlaylist(playlist, {
-//     name: "Georgia On My Mind",
-//     artist: 'Ray Charles',
-//     duration: 217,
-//     playCount: 0,
-//     youtubeLink: 'https://www.youtube.com/watch?v=ggGzE5KfCio'
-//   })) 
-//   console.log('playlist after addSongToPlaylist', copy(playlist))
-// }, 1000)
+window.setTimeout(() => {
+  console.log('addSongToPlaylist', addSongToPlaylist(playlist, {
+    name: "Georgia On My Mind",
+    artist: 'Ray Charles',
+    duration: 217,
+    playCount: 0,
+    youtubeLink: 'https://www.youtube.com/watch?v=ggGzE5KfCio'
+  })) 
+  console.log('playlist after addSongToPlaylist', copy(playlist))
+}, 1000)
 
 // ✅ Removing DOM elements
 
@@ -162,6 +186,7 @@ function removeSongFromPlaylist(playlist, youtubeLink) {
   const foundSongIndex = playlist.findIndex(song => song.youtubeLink === youtubeLink)
   if (foundSongIndex !== -1) {
     const songToRemove = playlist.splice(foundSongIndex, 1)[0];
+    songToRemove.element.remove()
     // Remove the song from playlist in the sidebar
     return songToRemove;
   } else {
@@ -171,10 +196,10 @@ function removeSongFromPlaylist(playlist, youtubeLink) {
 
 // // 👟👟👟 uncomment the lines below to test
 
-// window.setTimeout(() => {
-//   console.log('removeSongFromPlaylist', removeSongFromPlaylist(playlist, 'https://www.youtube.com/watch?v=ggGzE5KfCio'))
-//   console.log('playlist after addSongToPlaylist', copy(playlist))
-// }, 3000)
+window.setTimeout(() => {
+  console.log('removeSongFromPlaylist', removeSongFromPlaylist(playlist, 'https://www.youtube.com/watch?v=ggGzE5KfCio'))
+  console.log('playlist after addSongToPlaylist', copy(playlist))
+}, 3000)
 
 // ✅ Updating DOM elements
 
@@ -196,10 +221,10 @@ function extractVideoID(url) {
 // getPlayerElement()
 // Take care **NOT** to put the youtubeLink from the song directly into the src attribute for the iframe. We want it to be an embed version of the link and we want to make sure we're extracting the VideoID using the function defined above
 function loadSongIntoPlayer(song) {
-  getSongNameElement()
-  getArtistElement()
-  getPlayCountElement()
-  getPlayerElement()
+  getSongNameElement().innerText = song.name;
+  getArtistElement().innerText = song.artist;
+  getPlayCountElement().innerText = song.playCount === 1 ? '1 play' : `${song.playCount} plays`;
+  getPlayerElement().src = `https://www.youtube.com/embed/${extractVideoID(song.youtubeLink)}`
   //`https://www.youtube.com/embed/${extractVideoID(song.youtubeLink)}`;
 }
 
@@ -208,9 +233,9 @@ function loadSongIntoPlayer(song) {
 // // 👟👟👟 uncomment the lines below to test
 
 
-// loadSongIntoPlayer(playlist[0]);
-// loadSongIntoPlayer(playlist[1]);
-// loadSongIntoPlayer(playlist[2]);
+loadSongIntoPlayer(playlist[0]);
+loadSongIntoPlayer(playlist[1]);
+loadSongIntoPlayer(playlist[2]);
 // loadSongIntoPlayer(playlist[3]);
 // loadSongIntoPlayer(playlist[4]);
 // loadSongIntoPlayer(playlist[5]);
